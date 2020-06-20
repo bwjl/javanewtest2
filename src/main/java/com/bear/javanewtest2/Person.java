10171aaa81826c9f8e5a71e1afafc5b743c41a97
bear <593788753@qq.com>
2020-06-20 15:45:20 +0800

    构造函数默认生成 javap -c xxx 代码分析

---
+++ src/main/java/com/bear/javanewtest2/Person.java
@@ -0,0 +1,32 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/6/20 下午3:24
+ * Description:
+ */
+
+public class Person {
+
+    private String name;
+    private Integer age;
+
+    //当类中没有定义构造函数时，系统会指定给该类加上一个空参数的构造函数。这个是类中默认的构造函数。当类中如果自定义了构造函数，这时默认的构造函数就没有了
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/bear/javanewtest2/Singleton.java
@@ -0,0 +1,11 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/5/28 上午8:10
+ * Description: 单例模式
+ */
+
+public class Singleton {
+}
