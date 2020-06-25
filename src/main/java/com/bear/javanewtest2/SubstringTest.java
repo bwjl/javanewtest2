6174c501d184b029d0fbeb475cdfa07d3896b3a1
bear <593788753@qq.com>
2020-06-25 21:13:05 +0800

    substring 文件名后缀获取

---
+++ src/main/java/com/bear/javanewtest2/SubstringTest.java
@@ -0,0 +1,15 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/6/25 下午9:10
+ * Description:
+ */
+
+public class SubstringTest {
+    public static void main(String[] args) {
+        String fileName = "195454u6us36fab63s131f.jpeg";
+        System.out.println(fileName.substring(fileName.lastIndexOf(".")));
+    }
+}
