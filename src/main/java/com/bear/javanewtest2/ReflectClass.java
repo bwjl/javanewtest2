723c9858674c7543e5ce63381ffb8f262ad0c754
bear <593788753@qq.com>
2020-07-02 11:40:54 +0800

    一个类只有一个Class实例，无论通过哪种方式获取，得到的都是同一个Class对象

--- src/main/java/com/bear/javanewtest2/ReflectClass.java
+++ src/main/java/com/bear/javanewtest2/ReflectClass.java
@@ -13,7 +13,9 @@ public class ReflectClass {
         Class class1 = UserManagerImpl.class;
         Class class2 = Class.forName("com.bear.javanewtest2.UserManagerImpl");
         UserManager userManager = new UserManagerImpl();
+        UserManager userManager1 = new UserManagerImpl(); //一个类只有一个Class实例，无论通过哪种方式获取，得到的都是同一个Class对象
         Class class3 = userManager.getClass();
+        Class class4 = userManager1.getClass();
 
         System.out.println(class1);
         System.out.println(class2);
@@ -21,6 +23,7 @@ public class ReflectClass {
 
         System.out.println(class1 == class2);
         System.out.println(class2 == class3);
+        System.out.println(class3 == class4);
 
     }
 }
