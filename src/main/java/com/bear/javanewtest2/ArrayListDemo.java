c29adb5ae719e8b41c733db6156645b312a2130a
bear <593788753@qq.com>
2020-07-13 22:57:49 +0800

    arrayList

---
+++ src/main/java/com/bear/javanewtest2/ArrayListDemo.java
@@ -0,0 +1,29 @@
+package com.bear.javanewtest2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/7/13 下午10:49
+ * Description:
+ */
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("bear");
+        list.add("cat");
+        list.add("dog");
+        list.add("123");
+        list.add("456");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+    }
+
+}
