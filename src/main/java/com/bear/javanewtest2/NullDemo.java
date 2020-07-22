5b86e0d983d6601c8c7ef2a0ff575b73658efe15
bear <593788753@qq.com>
2020-07-22 12:04:03 +0800

    NullDemo

---
+++ src/main/java/com/bear/javanewtest2/NullDemo.java
@@ -0,0 +1,16 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/7/22 下午12:03
+ * Description:
+ */
+
+public class NullDemo {
+
+
+    public static void main(String[] args) {
+
+    }
+}
