ee4d26e82b0b8da649a3d49f85587eec05c21861
bear <593788753@qq.com>
2020-07-27 15:18:17 +0800

    array 定义与遍历

---
+++ src/main/java/com/bear/javanewtest2/ArrayDemo.java
@@ -0,0 +1,19 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/7/27 下午3:15
+ * Description:
+ */
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        Integer[] empIds = {640, 639};
+        //System.out.println(empIds);
+        for (int i = 0; i < empIds.length; i++) {
+            System.out.println(empIds[i]);
+        }
+    }
+}
