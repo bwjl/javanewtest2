a2b4b14cc867661dcd946e7327c0a556856e0110
bear <593788753@qq.com>
2020-08-19 00:03:19 +0800

    for (int i = 0...)

---
+++ src/main/java/com/bear/javanewtest2/ForInteger.java
@@ -0,0 +1,19 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/8/18 下午11:58
+ * Description: for (int i = 0...)
+ */
+
+public class ForInteger {
+
+    public static void main(String[] args) {
+
+//        for (Integer i = 0; i < 10; i++ ) {
+//
+//        }
+    }
+
+}
