1fa1c0b7f61025aab9510e65436917e311b9a3ee
bear <593788753@qq.com>
2020-08-20 11:35:52 +0800

    int 默认值0；Integer默认值null

---
+++ src/main/java/com/bear/javanewtest2/IntIntegerDefaultValueDemo.java
@@ -0,0 +1,23 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/8/20 上午11:33
+ * Description: int 默认值0；Integer默认值null
+ */
+
+public class IntIntegerDefaultValueDemo {
+
+    private static int a; //0
+    private static Integer b; //null
+
+    public static void main(String[] args) {
+
+//        int a;
+//        Integer b;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ src/main/java/com/bear/javanewtest2/StringChangeValue.java
@@ -0,0 +1,16 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/8/20 上午10:36
+ * Description:
+ */
+
+public class StringChangeValue {
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+
+    }
+}
