1d4be869cc3b0a66e331e7877a6129b049cdf00b
bear <593788753@qq.com>
2020-08-21 13:56:21 +0800

    字符串转byte[] 字符对应ASCII表数值

--- src/main/java/com/bear/javanewtest2/StringGetBytes.java
+++ src/main/java/com/bear/javanewtest2/StringGetBytes.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * Created with IntelliJ IDEA.
  * User: bear
  * Date: 2020/8/21 下午1:49
- * Description: 字符串转byte[]
+ * Description: 字符串转byte[] 字符对应ASCII表数值
  */
 
 public class StringGetBytes {
