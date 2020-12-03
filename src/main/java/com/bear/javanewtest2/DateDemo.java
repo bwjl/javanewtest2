4e71e94366f9d3c30f59e0b5d9cdef82b508f8e3
bear <593788753@qq.com>
2020-12-03 16:05:53 +0800

    new Date()用法 //Thu Dec 03 16:05:24 CST 2020

---
+++ src/main/java/com/bear/javanewtest2/DateDemo.java
@@ -0,0 +1,18 @@
+package com.bear.javanewtest2;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/12/3 下午3:41
+ * Description:
+ */
+
+public class DateDemo {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.toString()); //Thu Dec 03 16:05:24 CST 2020
+    }
+}
