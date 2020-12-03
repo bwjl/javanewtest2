a554b77e0ff02b86904d7834fe4b5fd05af90538
bear <593788753@qq.com>
2020-12-03 17:45:35 +0800

    声明为Object类型的形式参数

---
+++ src/main/java/com/bear/javanewtest2/ObjectTypeDemo.java
@@ -0,0 +1,20 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/12/3 下午5:42
+ * Description:
+ */
+
+public class ObjectTypeDemo {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        test(person);
+    }
+
+    public static void test(Object object) {
+        System.out.println(object);
+    }
+}
