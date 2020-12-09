e5f0d5c7997566a380a4fa857299f0a97500141e
bear <593788753@qq.com>
2020-12-09 15:27:40 +0800

    String与Integer 相互转换

---
+++ src/main/java/com/bear/javanewtest2/StringIntegerTransform.java
@@ -0,0 +1,26 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/12/9 下午3:21
+ * Description: String 与 Integer 相互转换
+ */
+
+public class StringIntegerTransform {
+
+    public static void main(String[] args) {
+
+
+        Integer b = 10;
+        System.out.println(String.valueOf(b));
+        System.out.println(b.toString());
+        System.out.println(Integer.toString(b));
+
+        String a = "1";
+        System.out.println(Integer.valueOf(a));
+
+
+    }
+
+}
