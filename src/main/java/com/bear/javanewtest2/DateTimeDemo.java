dae73685aa3fc26f024d64e1fdc5f3954d208dbe
bear <593788753@qq.com>
2020-12-12 21:02:05 +0800

    LocalDateTime 时间日期转换

---
+++ src/main/java/com/bear/javanewtest2/DateTimeDemo.java
@@ -0,0 +1,25 @@
+package com.bear.javanewtest2;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/12/12 下午8:50
+ * Description: 时间戳、日期转换
+ */
+
+public class DateTimeDemo {
+
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now()); //2020-12-12
+        System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))); //2020-12-12
+        System.out.println(LocalTime.now()); //20:54:22.543
+        System.out.println(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))); //20:58:16
+        System.out.println(LocalDateTime.now()); //2020-12-12T20:53:15.140
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))); //2020-12-12T20:53:15.140
+    }
+}
