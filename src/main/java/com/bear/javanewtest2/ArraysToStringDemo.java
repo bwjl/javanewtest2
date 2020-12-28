9500c5e6fff77d55cb1037c61bb4022f14d0562b
bear <593788753@qq.com>
2020-12-28 14:07:24 +0800

    Arrays.toString() 工具类

---
+++ src/main/java/com/bear/javanewtest2/ArraysToStringDemo.java
@@ -0,0 +1,22 @@
+package com.bear.javanewtest2;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/12/28 下午1:45
+ * Description:
+ */
+
+public class ArraysToStringDemo {
+
+    public static void main(String[] args) {
+
+        String[] paths = {"1", "24", "125"};
+
+        System.out.println(Arrays.toString(paths));
+
+    }
+
+}
