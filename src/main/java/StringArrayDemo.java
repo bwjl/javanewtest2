752524b4c08a86e38974b523e8554d9a75b21f72
bear <593788753@qq.com>
2021-01-19 10:07:23 +0800

    String 数组

---
+++ src/main/java/StringArrayDemo.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2021/1/19 上午10:05
+ * Description: String 数组
+ */
+
+public class StringArrayDemo {
+
+    public static void main(String[] args) {
+        String[] str = {"商品名称"};
+    }
+}
