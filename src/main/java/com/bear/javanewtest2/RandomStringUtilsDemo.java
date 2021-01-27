55d848e86b137b20b277bf6f721bb3f424b53e7e
bear <593788753@qq.com>
2021-01-27 17:50:04 +0800

    apache RandomStringUtils 随机字符串

---
+++ src/main/java/com/bear/javanewtest2/RandomStringUtilsDemo.java
@@ -0,0 +1,21 @@
+package com.bear.javanewtest2;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2021/1/27 下午5:40
+ * Description: apache RandomStringUtils 随机字符串
+ */
+
+public class RandomStringUtilsDemo {
+
+    public static void main(String[] args) {
+        String salt1 = RandomStringUtils.random(4);
+        //字母、数字
+        String salt2 = RandomStringUtils.randomAlphanumeric(4);
+        System.out.println(salt1);
+        System.out.println(salt2);
+    }
+}
