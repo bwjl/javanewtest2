540d9f776eafa85ebe8ff9b11f132c0f5bca80cd
bear <593788753@qq.com>
2021-01-28 19:02:54 +0800

    new Random() 随机数生成

--- src/main/java/com/bear/javanewtest2/RandomStringUtilsDemo.java
+++ src/main/java/com/bear/javanewtest2/RandomStringUtilsDemo.java
@@ -2,6 +2,9 @@ package com.bear.javanewtest2;
 
 import org.apache.commons.lang.RandomStringUtils;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Created with IntelliJ IDEA.
  * User: bear
@@ -16,8 +19,31 @@ public class RandomStringUtilsDemo {
         //字母、数字
         String salt2 = RandomStringUtils.randomAlphanumeric(4);
         String salt3 = RandomStringUtils.randomNumeric(4);
-        System.out.println(salt1);
-        System.out.println(salt2);
-        System.out.println(salt3);
+//        System.out.println(salt1);
+//        System.out.println(salt2);
+//        System.out.println(salt3);
+
+        System.out.println(randomAlphaNumber(6));
+    }
+
+    /**
+     * 随机生成数字字母
+     */
+    public static String randomAlphaNumber(int count) {
+        String str = "ABCDEFGHIJKMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789";
+        String[] strArray = str.split("");
+
+        //System.out.println(Arrays.toString(strArray));
+        //System.out.println(strArray);
+
+        Random random = new Random();
+
+        String randomStr = "";
+        for (int i = 0; i < count; i++) {
+            randomStr += strArray[random.nextInt(strArray.length)];
+        }
+
+        return randomStr;
+
     }
 }
