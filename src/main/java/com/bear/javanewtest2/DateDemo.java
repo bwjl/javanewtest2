f5df012bf1e03bc959895d8705c34df88aded4df
bear <593788753@qq.com>
2021-02-19 10:51:22 +0800

    Date 用法

--- src/main/java/com/bear/javanewtest2/DateDemo.java
+++ src/main/java/com/bear/javanewtest2/DateDemo.java
@@ -6,7 +6,7 @@ import java.util.Date;
  * Created with IntelliJ IDEA.
  * User: bear
  * Date: 2020/12/3 下午3:41
- * Description:
+ * Description: Date 用法
  */
 
 public class DateDemo {
@@ -14,5 +14,7 @@ public class DateDemo {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date.toString()); //Thu Dec 03 16:05:24 CST 2020
+        System.out.println(date.getTime()); //获取时间戳
+        System.out.println(System.currentTimeMillis());
     }
 }
