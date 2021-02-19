1563f65e7c3c77b996617a04b0b1d0a0b355d7f5
bear <593788753@qq.com>
2021-02-19 16:54:04 +0800

    Collection stream filter、distinct、skip、limit用法

--- src/main/java/com/bear/javanewtest2/StreamFilterDistinctSkipLimitDemo.java
+++ src/main/java/com/bear/javanewtest2/StreamFilterDistinctSkipLimitDemo.java
@@ -10,18 +10,20 @@ import java.util.stream.Collectors;
  * @Description: Collection stream filter、skip、limit用法
  */
 
-public class StreamFilterSkipLimitDemo {
+public class StreamFilterDistinctSkipLimitDemo {
 
     public static void main(String[] args) {
-        List<Integer> ints1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> ints1 = Arrays.asList(1, 2, 2, 3, 4, 5, 6);
         List<Integer> ints2 = ints1.stream().filter(num -> num > 2).collect(Collectors.toList());
         List<Integer> ints3 = ints1.stream().skip(3).collect(Collectors.toList());
         List<Integer> ints4 = ints1.stream().limit(2).collect(Collectors.toList());
+        List<Integer> ints5 = ints1.stream().distinct().collect(Collectors.toList());
 
         System.out.println(ints1);
         System.out.println(ints2);
         System.out.println(ints3);
         System.out.println(ints4);
+        System.out.println(ints5);
 
     }
 }
