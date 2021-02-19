7d9310c2877e194186d840177b25bf05e19f381d
bear <593788753@qq.com>
2021-02-19 10:52:15 +0800

    实体类 List<>

---
+++ src/main/java/com/bear/javanewtest2/entity/Users.java
@@ -0,0 +1,23 @@
+package com.bear.javanewtest2.entity;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2020/12/8 下午12:42
+ * Description:
+ */
+
+public class Users {
+
+    private List<String> users;
+
+    public List<String> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<String> users) {
+        this.users = users;
+    }
+}
