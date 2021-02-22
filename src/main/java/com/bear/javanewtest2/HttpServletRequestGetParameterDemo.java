b5d345b04574f7abbfe11c67affed074d5d91bf3
bear <593788753@qq.com>
2021-02-22 10:00:28 +0800

    HttpServlet

---
+++ src/main/java/com/bear/javanewtest2/HttpServletRequestGetParameterDemo.java
@@ -0,0 +1,11 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2021/1/6 下午3:21
+ * Description: TODO HttpServlet
+ */
+
+public class HttpServletRequestGetParameterDemo {
+}
