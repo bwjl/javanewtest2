c2d8d567f0acc6fc158789c220ad7a8fc96260c6
bear <593788753@qq.com>
2021-03-05 10:52:52 +0800

    Integer 获取最大值最小值

---
+++ src/main/java/com/bear/javanewtest2/IntegerMaxValue.java
@@ -0,0 +1,18 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/3/5 10:50 上午
+ * @Description: Integer 获取最大值最小值
+ */
+
+public class IntegerMaxValue {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+}
