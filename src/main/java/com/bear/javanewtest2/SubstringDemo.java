4baa2a98b9c51361f76f8c39e437870a6b6fe877
bear <593788753@qq.com>
2021-03-17 16:37:38 +0800

    (String).substring 截取字符串

--- src/main/java/com/bear/javanewtest2/SubstringDemo.java
+++ src/main/java/com/bear/javanewtest2/SubstringDemo.java
@@ -4,10 +4,10 @@ package com.bear.javanewtest2;
  * Created with IntelliJ IDEA.
  * User: bear
  * Date: 2020/6/25 下午9:10
- * Description:
+ * Description: (String).substring 截取字符串
  */
 
-public class SubstringTest {
+public class SubstringDemo {
     public static void main(String[] args) {
         String fileName = "195454u6us36fab63s131f.jpeg";
         System.out.println(fileName.substring(fileName.lastIndexOf(".")));
