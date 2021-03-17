13aa185194df2d9ba6e5055704404d2529209dc5
bear <593788753@qq.com>
2021-03-17 16:29:09 +0800

    split 正则点 点为正则关键字

---
+++ src/main/java/com/bear/javanewtest2/SplitRegexDemo.java
@@ -0,0 +1,21 @@
+package com.bear.javanewtest2;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/3/17 4:23 下午
+ * @Description: split 正则点 点为正则关键字
+ */
+
+public class SplitRegexDemo {
+
+    private static final String SRC_IMAGE = "/Users/bear/Desktop/wxcs1.jpg";
+
+    public static void main(String[] args) {
+        String[] ret = SRC_IMAGE.split("\\.");
+        System.out.println(Arrays.toString(ret));
+    }
+}
