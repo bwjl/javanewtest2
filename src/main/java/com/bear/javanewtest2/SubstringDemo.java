578e4046ea38153b64bd567c0a785c6a1422e010
bear <593788753@qq.com>
2021-03-17 16:55:19 +0800

    (String).substring 截取字符串 获取文件后缀

--- src/main/java/com/bear/javanewtest2/SubstringDemo.java
+++ src/main/java/com/bear/javanewtest2/SubstringDemo.java
@@ -4,12 +4,12 @@ package com.bear.javanewtest2;
  * Created with IntelliJ IDEA.
  * User: bear
  * Date: 2020/6/25 下午9:10
- * Description: (String).substring 截取字符串
+ * Description: (String).substring 截取字符串 获取文件后缀
  */
 
 public class SubstringDemo {
     public static void main(String[] args) {
         String fileName = "195454u6us36fab63s131f.jpeg";
-        System.out.println(fileName.substring(fileName.lastIndexOf(".")));
+        System.out.println(fileName.substring(fileName.lastIndexOf(".") + 1));
     }
 }
