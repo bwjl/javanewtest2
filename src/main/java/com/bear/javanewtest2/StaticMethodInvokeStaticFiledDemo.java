91fcd59b19b60694d76c9455f10bd777ccd1f5d2
bear <593788753@qq.com>
2021-05-17 15:57:39 +0800

    静态方法只能调用本类字段必须为静态

---
+++ src/main/java/com/bear/javanewtest2/StaticMethodInvokeStaticFiledDemo.java
@@ -0,0 +1,20 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/5/17 3:55 下午
+ * @Description: 静态方法调用静态字段
+ */
+
+public class StaticMethodInvokeStaticFiledDemo {
+
+    private String name = "bear";
+
+    public static void main(String[] args) {
+
+        //System.out.println(name); //静态方法只能调用本类字段必须为静态
+    }
+
+}
