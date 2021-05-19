a02572a87121f4c360909719eb0668fd714f2415
bear <593788753@qq.com>
2021-05-19 16:45:21 +0800

    long类型除法 返回小数自动舍去

---
+++ src/main/java/com/bear/javanewtest2/LongDivDemo.java
@@ -0,0 +1,16 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/5/19 4:40 下午
+ * @Description: long类型除法 返回小数自动舍去
+ */
+
+public class LongDivDemo {
+
+    public static void main(String[] args) {
+        System.out.println(155245644001L / 1000L); //155245644
+    }
+}
