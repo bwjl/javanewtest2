1431c26088c2dd2c63ee094c209efbd50b212237
bear <593788753@qq.com>
2021-05-20 11:58:06 +0800

    public字段 people.name 直接获取

---
+++ src/main/java/com/bear/javanewtest2/EasyExcelDemo.java
@@ -0,0 +1,19 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bear
+ * Date: 2021/1/19 下午3:33
+ * Description:
+ */
+
+public class EasyExcelDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public static void export(HttpServletRequest request, HttpServletResponse response) {
+//
+//    }
+}
---
+++ src/main/java/com/bear/javanewtest2/constants/People.java
@@ -0,0 +1,14 @@
+package com.bear.javanewtest2.constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/5/20 11:55 上午
+ * @Description:
+ */
+
+public class People {
+
+    public String name = "bear";
+}
