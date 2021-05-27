1e06a9d6df552ff65b4a05e52754556506030597
bear <593788753@qq.com>
2021-05-27 16:06:50 +0800

    string.contains 包含某段字符串判断

--- src/main/java/com/bear/javanewtest2/strings/StringContainsDemo.java
+++ src/main/java/com/bear/javanewtest2/strings/StringContainsDemo.java
@@ -5,8 +5,17 @@ package com.bear.javanewtest2.strings;
  *
  * @Author: bear
  * @Date: 2021/3/19 4:33 下午
- * @Description:
+ * @Description: string.contains 包含某段字符串判断
  */
 
 public class StringContainsDemo {
+
+
+    public static void main(String[] args) {
+        String contentType = "multipart/form-data; boundary=--------------------------907201013825265333914340";
+
+        System.out.println(contentType.contains("multipart"));
+        System.out.println(contentType.contains("x-www-form-urlencoded"));
+    }
+
 }
