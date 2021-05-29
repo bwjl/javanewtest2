8c5298d5f435a24862a52bf6eeede87a50efe968
bear <593788753@qq.com>
2021-05-29 23:14:31 +0800

    Arrays.toString(Byte[])

--- src/main/java/com/bear/javanewtest2/StringGetBytes.java
+++ src/main/java/com/bear/javanewtest2/StringGetBytes.java
@@ -1,12 +1,13 @@
 package com.bear.javanewtest2;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 /**
  * Created with IntelliJ IDEA.
  * User: bear
  * Date: 2020/8/21 下午1:49
- * Description: 字符串转byte[] 字符对应ASCII表数值
+ * Description: 字符串转byte[] 字符对应ASCII表数值 Arrays.toString(Byte[])
  */
 
 public class StringGetBytes {
@@ -15,6 +16,9 @@ public class StringGetBytes {
 
         String tips = "hello world";
         System.out.println(Arrays.toString(tips.getBytes()));
+        System.out.println(tips.getBytes());
+        String name = "您好，熊大";
+        System.out.println(Arrays.toString(name.getBytes(StandardCharsets.UTF_8)));
 
     }
 }
