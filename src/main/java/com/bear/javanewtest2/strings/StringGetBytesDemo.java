4a0d5cc4d82711e6f1edc6c156f74d5d79ad0bae
bear <593788753@qq.com>
2021-05-30 12:10:01 +0800

    验证GBK、GB2312两个字节，UTF-8一个字节

---
+++ src/main/java/com/bear/javanewtest2/strings/StringGetBytesDemo.java
@@ -0,0 +1,29 @@
+package com.bear.javanewtest2.strings;
+
+import lombok.SneakyThrows;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/5/30 12:04 下午
+ * @Description: 验证GBK、GB2312两个字节，UTF-8一个字节
+ */
+
+
+public class StringGetBytesDemo {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        String name = "熊";
+        System.out.println(Arrays.toString(name.getBytes(StandardCharsets.UTF_8)));
+        //GBK 简体中文、繁体中文
+        System.out.println(Arrays.toString(name.getBytes("GBK")));
+        //GB2312 仅简体中文
+        System.out.println(Arrays.toString(name.getBytes("GB2312")));
+    }
+}
