20dd469fc16f7f03b2b5c5c1c2794a64d6fc348c
bear <593788753@qq.com>
2021-06-04 16:32:42 +0800

    时间格式化 大小写

--- src/main/java/com/bear/javanewtest2/SimpleDateFormatDemo.java
+++ src/main/java/com/bear/javanewtest2/SimpleDateFormatDemo.java
@@ -13,6 +13,21 @@ import java.util.Date;
 public class SimpleDateFormatDemo {
 
     public static void main(String[] args) {
+
+//   1、yy表示Year, Y表示Week Year，计算方式不一样，通常使用【y】
+//
+//　　2、MM是为了和分钟的m区分，Month in year
+//
+//　　3、dd是Day in month，D是Day in year
+//
+//　　4、HH是24小时制Hour in day,hh是12小时制Hour in am/pm
+//
+//　　5、mm是分，Minute in hour
+//
+//　　6、ss是秒Second in minute，S是Millisecond
+//
+//　　7、aa是Am/pm marker
+
         //System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         String date1 = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss").format(new Date());
