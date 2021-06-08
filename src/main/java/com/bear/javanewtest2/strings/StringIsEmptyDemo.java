7fd67b0087b128ba38422ea6b4c094555334ac46
bear <593788753@qq.com>
2021-06-08 16:28:44 +0800

    isEmpty()判断字符串长度为0

---
+++ src/main/java/com/bear/javanewtest2/strings/StringIsEmptyDemo.java
@@ -0,0 +1,17 @@
+package com.bear.javanewtest2.strings;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/6/8 4:26 下午
+ * @Description: isEmpty()判断字符串长度为0
+ */
+
+public class StringIsEmptyDemo {
+
+    public static void main(String[] args) {
+        String name = "bear";
+        System.out.println(name.isEmpty());
+    }
+}
