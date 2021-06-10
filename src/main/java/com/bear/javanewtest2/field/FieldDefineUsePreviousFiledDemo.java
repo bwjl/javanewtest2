e04bcba7b0ea34902ec863d6d0f77485bb3dc08b
bear <593788753@qq.com>
2021-06-10 20:25:10 +0800

    类字段可以调用上一个字段（注意顺序）

---
+++ src/main/java/com/bear/javanewtest2/field/FieldDefineUsePreviousFiledDemo.java
@@ -0,0 +1,20 @@
+package com.bear.javanewtest2.field;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/6/10 8:19 下午
+ * @Description:
+ */
+
+public class FieldDefineUsePreviousFiledDemo {
+
+    private static String root = "root";
+
+    private static String path = root + "/path";
+
+    public static void main(String[] args) {
+        System.out.println(path);
+    }
+}
