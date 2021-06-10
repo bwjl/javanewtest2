71fc36a15676062b4707c046b7ca1d1570514a35
bear <593788753@qq.com>
2021-06-10 10:25:22 +0800

    UUID 生成

--- src/main/java/com/bear/javanewtest2/randomUUIDDemo.java
+++ src/main/java/com/bear/javanewtest2/randomUUIDDemo.java
@@ -6,12 +6,12 @@ import java.util.UUID;
  * Created with IntelliJ IDEA.
  * User: bear
  * Date: 2020/6/25 下午9:19
- * Description:
+ * Description: UUID 生成
  */
 
-public class randomUUIDTest {
+public class randomUUIDDemo {
     public static void main(String[] args) {
-        System.out.println(UUID.randomUUID().toString()); //aee13330-46a5-41c5-833e-91590a54d422
+        System.out.println(UUID.randomUUID()); //aee13330-46a5-41c5-833e-91590a54d422
         System.out.println(UUID.randomUUID().toString().replace("-", "")); //e29e3d61b47948a0b4a661a050715b1f
     }
 }
