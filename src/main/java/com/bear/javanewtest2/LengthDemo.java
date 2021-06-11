4a1f4882a337868b9b56d01877637fb494393d77
bear <593788753@qq.com>
2021-06-11 10:47:33 +0800

    字符串 length()方法；数组 length字段

---
+++ src/main/java/com/bear/javanewtest2/LengthDemo.java
@@ -0,0 +1,20 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/6/11 10:44 上午
+ * @Description: 字符串 length()方法；数组 length字段
+ */
+
+public class LengthDemo {
+
+    public static void main(String[] args) {
+        String name = "bear";
+        System.out.println(name.length());
+
+        String[] hobby = {"sleep", "drink", "eat"};
+        System.out.println(hobby.length);
+    }
+}
