4b19079fc05e7d90817f28e36acd95e896cb2e28
bear <593788753@qq.com>
2021-06-23 11:59:36 +0800

    遍历文件夹文件获取文件名、并且重命名

--- src/main/java/com/bear/javanewtest2/file/IterateFolderFileDemo.java
+++ src/main/java/com/bear/javanewtest2/file/IterateFolderFileDemo.java
@@ -9,18 +9,33 @@ import java.util.Objects;
  *
  * @Author: bear
  * @Date: 2021/6/23 10:43 上午
- * @Description: 遍历文件夹文件获取文件名
+ * @Description: 遍历文件夹文件获取文件名、并且重命名
  */
 
 public class IterateFolderFileDemo {
 
     private static final String FOLDER_NAME = "/Users/bear/Desktop/gedian/20210623";
+    private static final String[] ALLOWED_FILE_EXT = {"jpg", "jpeg", "png"};
 
     public static void main(String[] args) {
         File folder = new File(FOLDER_NAME);
         File[] files = folder.listFiles();
         for (int i = 0; i < Objects.requireNonNull(files).length; i++) {
-            System.out.println(files[i].getName());
+            if (files[i].isFile()) {
+                //是文件
+                String fileName = files[i].getName();
+                System.out.println(fileName);
+
+                String[] FileArr = fileName.split("\\.");
+
+                //允许的后缀
+                if (Arrays.asList(ALLOWED_FILE_EXT).contains(FileArr[FileArr.length - 1])) {
+                    String newName = fileName.substring(0, fileName.lastIndexOf(".")) + "-1"
+                            + fileName.substring(fileName.lastIndexOf("."), fileName.length());
+                    System.out.println(newName);
+                }
+
+            }
         }
     }
 }
