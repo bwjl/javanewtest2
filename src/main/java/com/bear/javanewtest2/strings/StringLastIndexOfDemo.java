69cc16236b83bc9f030f22bafe9cd1efee1d76ae
bear <593788753@qq.com>
2021-06-23 11:41:31 +0800

    字符串最后出现的位置 String::lastIndexOf()

---
+++ src/main/java/com/bear/javanewtest2/strings/StringLastIndexOfDemo.java
@@ -0,0 +1,17 @@
+package com.bear.javanewtest2.strings;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/6/23 11:24 上午
+ * @Description: 字符串最后出现的位置
+ */
+
+public class StringLastIndexOfDemo {
+
+    public static void main(String[] args) {
+        String fileName = "195454u6us36fab63s131f.jpeg";
+        System.out.println(fileName.lastIndexOf("."));
+    }
+}
