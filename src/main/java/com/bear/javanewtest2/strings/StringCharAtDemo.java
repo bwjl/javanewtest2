add231be091be380d1a135ae7153cc9660f73ca3
bear <593788753@qq.com>
2021-07-02 11:03:42 +0800

    String::charAt 获取某位置的字符

---
+++ src/main/java/com/bear/javanewtest2/strings/StringCharAtDemo.java
@@ -0,0 +1,20 @@
+package com.bear.javanewtest2.strings;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/7/2 11:00 上午
+ * @Description: String::charAt 获取某位置的字符
+ */
+
+public class StringCharAtDemo {
+
+    public static void main(String[] args) {
+        String name = "bear dog cat";
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.charAt(2));
+        System.out.println(name.charAt(3));
+    }
+}
