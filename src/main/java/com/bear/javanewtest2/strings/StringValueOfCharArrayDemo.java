d7ed340bcb62d44ce3ebb43795a46e585c29934c
bear <593788753@qq.com>
2021-07-14 15:37:19 +0800

    String.valueOf(char[]) char数组转字符串

---
+++ src/main/java/com/bear/javanewtest2/strings/StringValueOfCharArrayDemo.java
@@ -0,0 +1,18 @@
+package com.bear.javanewtest2.strings;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/7/14 3:33 下午
+ * @Description: String.valueOf(char[]) char数组转字符串
+ */
+
+public class StringValueOfCharArrayDemo {
+
+    public static void main(String[] args) {
+        char[] zjj = {'z', 'j', 'j'};
+        System.out.println(zjj);
+        System.out.println(String.valueOf(zjj));
+    }
+}
