f4c3845d91882de964ae5cda0bb878ab882719a5
bear <593788753@qq.com>
2021-07-15 18:53:04 +0800

    float 0 Float 0f

---
+++ src/main/java/com/bear/javanewtest2/FloatZeroDemo.java
@@ -0,0 +1,17 @@
+package com.bear.javanewtest2;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/7/15 6:50 下午
+ * @Description: float 0 Float 0f
+ */
+
+public class FloatZeroDemo {
+
+    public static void main(String[] args) {
+        float price1 = 0;
+        Float price = 0f;
+    }
+}
